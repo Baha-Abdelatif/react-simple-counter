733a24adb0163b649c095cbf36e160228ffdf072
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-06 22:27:23 +0200

    Use memo and useCallback Hook to avoid re-renders of increment/decrement buttons.

--- src/components/Counter/CounterOutput.jsx
+++ src/components/Counter/CounterOutput.jsx
@@ -1,8 +1,13 @@
-import { log } from '../../log.js';
+import { log } from "../../log.js";
+import { PropTypes } from "prop-types";
 
 export default function CounterOutput({ value }) {
-  log('<CounterOutput /> rendered', 2);
+  log("<CounterOutput /> rendered", 2);
 
-  const cssClass = value >= 0 ? 'counter-output' : 'counter-output negative';
+  const cssClass = value >= 0 ? "counter-output" : "counter-output negative";
   return <span className={cssClass}>{value}</span>;
 }
+
+CounterOutput.propTypes = {
+  value: PropTypes.number,
+};
--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,13 +1,21 @@
-import { log } from '../../log.js';
+import { memo } from "react";
+import { PropTypes } from "prop-types";
+import { log } from "../../log.js";
 
-export default function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
+  log("<IconButton /> rendered", 2);
 
   const Icon = icon;
   return (
-    <button {...props} className="button">
-      <Icon className="button-icon" />
-      <span className="button-text">{children}</span>
+    <button {...props} className='button'>
+      <Icon className='button-icon' />
+      <span className='button-text'>{children}</span>
     </button>
   );
-}
+});
+export default IconButton;
+
+IconButton.propTypes = {
+  children: PropTypes.any,
+  icon: PropTypes.func,
+};
